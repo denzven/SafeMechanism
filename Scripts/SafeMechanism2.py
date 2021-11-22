c24600c0a9e52465d7bc44efaa5f049eec4b18c9
denzven <denzvenvadakkan@gmail.com>
2021-11-22 22:17:02 +0530

    better encrypt

--- Scripts/SafeMechanism2.py
+++ Scripts/SafeMechanism2.py
@@ -10,78 +10,112 @@ import tkinter.messagebox
 import sys
 import os
 import base64
+import pyAesCrypt as aes
 
+def DenzEncrypt(InputFile,InputPin):
+    global PIN
+    global PassKey
+
+    buffer = 64 * 1024
+    PathToFile,FileExt = os.path.splitext(InputFile)
+    OutputFile1 = f"{PathToFile}.denz~"
+    OutputFile = f"{PathToFile}.denz"
+    ConfigText = f"####DenzEncrypt####|{InputPin}|{PathToFile}|{InputFile}|{OutputFile}|####\n"
+    with open(InputFile,'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.write(ConfigText)
+        for line in lines:
+            f.write(line)
+        f.close()
+    aes.encryptFile(InputFile,OutputFile1,InputPin,buffer)
+    base64.encode(open(OutputFile1,"rb"),open(OutputFile,"wb"))
+    os.remove(InputFile)
+    os.remove(OutputFile1)
+
+def DenzDecrypt(InputFile,InputPin):
+    global PIN
+    global PassKey
+    buffer = 64 * 1024
+    PathToFile,FileExt = os.path.splitext(InputFile)
+    OutputFile1 = f"{PathToFile}.denz~"
+    OutputFile = f"{PathToFile}.txt"
+    base64.decode(open(InputFile,"rb"),open(OutputFile1,"wb"))
+    try:
+        aes.decryptFile(OutputFile1,OutputFile,InputPin,buffer)
+    except Exception:
+        PIN = ''
+        e.delete('0', 'end')
+        os.remove(OutputFile1)
+        tkinter.messagebox.showerror('PIN ERROR!', 'PIN entered is Incorrect!')
+
+    with open(OutputFile,'r+') as f:
+        FirstLine = f.readline()
+        print(FirstLine)
+        FileInfo = FirstLine.split("|")
+        if FileInfo[0] == "####DenzEncrypt####":
+            PassKey = FileInfo[1]
+            #PathToFile = FileInfo[2]
+            #InputFile = FileInfo[3]
+            #OutputFile = FileInfo[4]
+            return PassKey
+    
 def EncryptFile(InputFile,InputPin):
     global PIN
     global PassKey
     global SafeContent
+    buffer = 64 * 1024
     """INFO INFO.
 
     info
     """
 
     PathToFile,FileExt = os.path.splitext(InputFile)
-    print(FileExt)
     if FileExt == '.txt':
         if len(PIN) >= 3 and len(PIN) <= 6:
             print("PIN SET")
             PIN = ''
             e.delete('0', 'end')
             tkinter.messagebox.showinfo('PIN SET', f'PIN has been set to: {InputPin}')            
-            with open(InputFile,'r+') as f:
-                lines = f.readlines()
-                f.seek(0)
-                f.write("#### Passwd: "+ InputPin + " ####" + "\n")
-                for line in lines:
-                    f.write(line)
-                f.close()
-            OutputFile = f"{PathToFile}.denz"
-            base64.encode(open(InputFile,"rb"),open(OutputFile,"wb"))
+            DenzEncrypt(InputFile,InputPin)
             tkinter.messagebox.showinfo('Response', f'Okay, {InputFile} has been locked sucessfully')
-            os.remove(InputFile)
             root.destroy()
 
         else:
             print("PIN NOT VALID!")
             print("INVALID PIN: " + PIN)
-            # clear `PIN`
             PIN = ''
-            # clear `entry`
             e.delete('0', 'end')
             tkinter.messagebox.showerror('PIN NOT VALID!', 'PIN entered is invalid, the PIN must be between 3-6 chars long!')
 
     elif FileExt == '.denz':
         OutputFile = f"{PathToFile}.txt"
-        base64.decode(open(InputFile,"rb"),open(OutputFile,"wb"))
-        with open(OutputFile,'r+') as f:
-            FirstLine = f.readline()
-            print(FirstLine)
-            if FirstLine.startswith("#### Passwd: "):
-                print(FirstLine)
-                PassKey = FirstLine[13:-6]
-                print(PassKey)
-                if InputPin == PassKey:
-                    with open(OutputFile, 'r') as f:
-                        data = f.read().splitlines(True)
-                    with open(OutputFile, 'w') as f:
-                        f.writelines(data[1:])
-                    print("PIN OK")
-                    print("OK PIN: " + PIN)
-                    os.remove(InputFile)
-                    PIN = ''
-                    e.delete('0', 'end')
-                    tkinter.messagebox.showinfo('PIN OK', f'PIN is Correct! {InputFile} unlocked')
-                    PassKey = None
-                    root.destroy()
+        OutputFile1 = f"{PathToFile}.denz~"
+        PassKey = DenzDecrypt(InputFile,InputPin)
+        if InputPin == PassKey:
+            with open(OutputFile, 'r') as f:
+                data = f.read().splitlines(True)
+            with open(OutputFile, 'w') as f:
+                f.writelines(data[1:])
+                os.remove(InputFile)
+                os.remove(OutputFile1)
+            print("PIN OK")
+            print("OK PIN: " + PIN)
+            PIN = ''
+            e.delete('0', 'end')
+            tkinter.messagebox.showinfo('PIN OK', f'PIN is Correct! {InputFile} unlocked')
+            PassKey = None
+            root.destroy()
 
-                else:
-                    print("PIN ERROR2!")
-                    print("ERROR PIN: " + PIN)
-                    PIN = ''
-                    e.delete('0', 'end')
-                    tkinter.messagebox.showerror('PIN ERROR!', 'PIN entered is Incorrect!')
-                    os.remove(OutputFile)
-                    EncryptFile(InputFile,Passwd)
+        else:
+            print("PIN ERROR2!")
+            print("ERROR PIN: " + PIN)
+            PIN = ''
+            e.delete('0', 'end')
+            tkinter.messagebox.showerror('PIN ERROR!', 'PIN entered is Incorrect!')
+            #os.remove(OutputFile)
+            #os.remove(OutputFile1)
+            EncryptFile(InputFile,PassKey)
 
     else:
         tkinter.messagebox.showwarning('File Error!', 'InputFile is not a .txt file!')
@@ -113,7 +147,7 @@ def GetUserInput():
     top = tk.Toplevel()
     top.title("User Input")
     try:
-        top.iconbitmap("..\\Icons\\SafeMechaismIcon.ico")
+        top.iconbitmap("Icons\\SafeMechaismIcon.ico")
         pass
     except Exception as e:
         pass
@@ -179,7 +213,6 @@ def MainSafe(value):
                 tkinter.messagebox.showerror('PIN NOT VALID!', 'PIN entered is invalid, the PIN must be between 3-6 chars long!')
 
         elif sys.argv[1] is not None:
-            print("jmm")
             EncryptFile(sys.argv[1],PIN)
 
         else:
@@ -222,7 +255,7 @@ root = tk.Tk()
 root.title("SafeMechaism(GUI)")
 root.geometry('350x450+700+200')
 try:
-    top.iconbitmap("..\\Icons\\SafeMechaismIcon.ico")
+    top.iconbitmap("Icons\\SafeMechaismIcon.ico")
     pass
 except Exception as e:
     pass
